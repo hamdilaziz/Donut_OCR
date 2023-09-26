c4659f7a61d5103ac3d6af2e6e0c931d2459da26
Laziz Hamdi <34897923+hamdilaziz@users.noreply.github.com>
2023-09-27 00:00:03 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -41,15 +41,15 @@ test_names = list(test_set.keys())
 
 # parameters
 config = {
-  "part":"all",
+  "part":"decoder_encoder_all",
   "mean":[0.485, 0.456, 0.406],
   "std":[0.229, 0.224, 0.225],
   "image_size":[1920, 2560],
   "max_length":224,
   "batch_size":3,
-  "learning_rate":5e-7,
+  "learning_rate":1e-6,
   "device":'cuda' if torch.cuda.is_available() else 'cpu',
-  "epochs":10
+  "epochs":40
 }
 
 # dataset
@@ -95,7 +95,7 @@ valid_indices = DataLoader(range(len(valid_dataset)), batch_size=config['batch_s
 
 # Load the model 
 processor = DonutProcessor.from_pretrained("/gpfsstore/rech/jqv/ubb84id/huggingface_models/donut/processor")
-model = VisionEncoderDecoderModel.from_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/donut_all_lr1e-06_h2560_w1920")
+model = VisionEncoderDecoderModel.from_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/donut_encoder_after_decoder_lr1e-06_h2560_w1920")
 
 processor.image_processor.size = config['image_size']
 processor.image_processor.mean = config['mean']
@@ -163,7 +163,7 @@ for epoch in tqdm(range(config['epochs'])):
                 print("img size {}, Train loss {}, valid loss {}, cer {}, wer {}".format(x_valid.shape, train_loss.mean().item(), valid_loss, cer, wer))
             if valid_loss < best_valid_loss:
                 best_valid_loss = valid_loss
-                output_folder_name = "donut_all_lr{}_h{}_w{}".format(config['learning_rate'], config['image_size'][1], config['image_size'][0])
+                output_folder_name = "donut_decoder_encoder_all_lr{}_h{}_w{}".format(config['learning_rate'], config['image_size'][1], config['image_size'][0])
                 model.save_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/"+output_folder_name)
                 with open("/gpfsstore/rech/jqv/ubb84id/output_models/"+output_folder_name+"/info.txt", "w") as f:
                     f.write("checkpoints created at step: {} with train loss : {} and valid loss : {}".format(step, train_loss, best_valid_loss))
