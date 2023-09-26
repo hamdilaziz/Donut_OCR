f714945b7fcd895d7e4c5022ea7ad33a7e08ff46
Laziz Hamdi <34897923+hamdilaziz@users.noreply.github.com>
2023-09-26 13:30:55 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -46,10 +46,10 @@ config = {
   "std":[0.229, 0.224, 0.225],
   "image_size":[1920, 2560],
   "max_length":224,
-  "batch_size":2,
+  "batch_size":3,
   "learning_rate":1e-6,
   "device":'cuda' if torch.cuda.is_available() else 'cpu',
-  "epochs":40
+  "epochs":30
 }
 
 # dataset
@@ -95,7 +95,7 @@ valid_indices = DataLoader(range(len(valid_dataset)), batch_size=config['batch_s
 
 # Load the model 
 processor = DonutProcessor.from_pretrained("/gpfsstore/rech/jqv/ubb84id/huggingface_models/donut/processor")
-model = VisionEncoderDecoderModel.from_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/donut_encoder_after_decoder_lr1e-06_h2560_w1920")
+model = VisionEncoderDecoderModel.from_pretrained("/gpfsstore/rech/jqv/ubb84id/huggingface_models/donut/model")
 
 processor.image_processor.size = config['image_size']
 processor.image_processor.mean = config['mean']
@@ -158,12 +158,12 @@ for epoch in tqdm(range(config['epochs'])):
                 tokens = tokenizer.convert_ids_to_tokens(y_valid)
                 text = tokenizer.convert_tokens_to_string([t for t in tokens if t not in sepcial_tokens])
                 cer = edit_cer_from_string(text, pred_text)/len(text)
-                wer = edit_wer_from_string(text, pred_text)/len(text)
+                wer = edit_wer_from_string(text, pred_text)/len(text.split())
                 run.log({"train_loss":train_loss.mean().item(), "valid_loss":valid_loss, "cer":cer, "wer":wer})
                 print("img size {}, Train loss {}, valid loss {}, cer {}, wer {}".format(x_valid.shape, train_loss.mean().item(), valid_loss, cer, wer))
             if valid_loss < best_valid_loss:
                 best_valid_loss = valid_loss
-                output_folder_name = "donut_encoder_decoder_all_lr{}_h{}_w{}".format(config['learning_rate'], config['image_size'][1], config['image_size'][0])
+                output_folder_name = "donut_all_lr{}_h{}_w{}".format(config['learning_rate'], config['image_size'][1], config['image_size'][0])
                 model.save_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/"+output_folder_name)
                 with open("/gpfsstore/rech/jqv/ubb84id/output_models/"+output_folder_name+"/info.txt", "w") as f:
                     f.write("checkpoints created at step: {} with train loss : {} and valid loss : {}".format(step, train_loss, best_valid_loss))
