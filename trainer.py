a35ca02de8e17bc672d4d463922e3765c8ab3155
Laziz Hamdi <34897923+hamdilaziz@users.noreply.github.com>
2023-09-29 00:51:28 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -41,7 +41,7 @@ test_names = list(test_set.keys())
 
 # parameters
 config = {
-  "part":"decoder_encoder",
+  "part":"encoder_decoder",
   "mean":[0.485, 0.456, 0.406],
   "std":[0.229, 0.224, 0.225],
   "image_size":[1920, 2560],
@@ -95,7 +95,7 @@ valid_indices = DataLoader(range(len(valid_dataset)), batch_size=config['batch_s
 
 # Load the model 
 processor = DonutProcessor.from_pretrained("/gpfsstore/rech/jqv/ubb84id/huggingface_models/donut/processor")
-model = VisionEncoderDecoderModel.from_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/decoder_lr1e-06_h2560_w1920")
+model = VisionEncoderDecoderModel.from_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/encoder_lr1e-06_h2560_w1920")
 
 processor.image_processor.size = config['image_size']
 processor.image_processor.mean = config['mean']
@@ -121,13 +121,13 @@ os.environ["WANDB_MODE"]="offline"
 wandb.login(key = KEY)
 run = wandb.init(project="Donut",
                  entity="lazizhamdi",
-                 id = '3fmlp0qi',
+                 id = '2dlgyy3l',
                  config=config)
 
 # training forloop
 model.to(config['device'])
-# train only the decoder
-for p in model.decoder.parameters():
+# train only the encoder
+for p in model.encoder.parameters():
     p.requires_grad = False
   
 opt = AdamW(model.parameters(), lr=config['learning_rate'])
@@ -164,7 +164,7 @@ for epoch in tqdm(range(config['epochs'])):
                 print("img size {}, Train loss {}, valid loss {}, cer {}, wer {}".format(x_valid.shape, train_loss.mean().item(), valid_loss, cer, wer))
             if valid_loss < best_valid_loss:
                 best_valid_loss = valid_loss
-                output_folder_name = "decoder_encoder_lr{}_h{}_w{}".format(config['learning_rate'], config['image_size'][1], config['image_size'][0])
+                output_folder_name = "encoder_decoder_lr{}_h{}_w{}".format(config['learning_rate'], config['image_size'][1], config['image_size'][0])
                 model.save_pretrained("/gpfsstore/rech/jqv/ubb84id/output_models/"+output_folder_name)
                 with open("/gpfsstore/rech/jqv/ubb84id/output_models/"+output_folder_name+"/info.txt", "w") as f:
                     f.write("checkpoints created at step: {} with train loss : {} and valid loss : {}".format(step, train_loss, best_valid_loss))
